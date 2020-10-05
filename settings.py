4348a3f582a5d8f9df6c395e864d0959b24d7ba9
Adam Reed <71365324+areed06@users.noreply.github.com>
2020-10-05 00:00:48 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
 build_num = "1.0.0"  # current build version
-available_modes = ("Gaussian", "Poisson", "Salt & Pepper", "Adam's Custom Algorithm")
+
+mode_activations = {"Salt & Pepper": 1, "Gaussian": 0, "Poisson": 0, "Adam's Custom Algorithm": 0}
+available_modes = ("Salt & Pepper", "Gaussian", "Poisson", "Adam's Custom Algorithm")
+
 min_window_width = 800
 min_window_height = 500
 
@@ -8,3 +11,4 @@ button_font_size = 10
 combobox_font_size = 10
 
 supported_files = [('JPEG Files', '*.jpg*'), ('NEF Raw Files', '*.NEF*'), ('TIF Files', '*.tif*')]
+default_save_directory = 'C:\\'
